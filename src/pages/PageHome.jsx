fa71183a2e279d0e8fc9b9940e9aae18081aeb79
junior gomez <junior.gomez@itconsultores.com.co>
2023-08-03 12:12:41 -0500

    se agrego el footer y se le hicieron correcciones a el header

--- src/pages/PageHome.jsx
+++ src/pages/PageHome.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import LibertyHeader from "../components/basic/LibertyHeader/LibertyHeader";
 import images from "../components/basic/LibertyHeader/icons/images.jpeg";
 import PelimaxCard from "../components/PelimaxCard/PelimaxCard";
+import PeliMaxFooter from "../components/basic/PeliMaxFooter/PeliMaxFooter";
 
 const PageHome = () => {
   return (
     <div>
       <LibertyHeader />
       <PelimaxCard src={images} />
+      <PeliMaxFooter />
     </div>
   );
 };
